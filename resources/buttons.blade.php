29f420b6d13b08b16b57f53678e0f52d4a3b9c6e
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-21 01:27:22 -0400

    uploading the files I changed.

---
+++ resources/buttons.blade.php
@@ -0,0 +1,13 @@
+    @extends('layouts.app')
+
+@section('buttons')   
+    <div class ="btnlistTextCenter">
+          <div class ="twitter"><a  href="*"><i class="fa fa-twitter fa-lg" id="twitterIcon" aria-hidden="true"></i>
+Twitter</a></div>&nbsp&nbsp&nbsp
+         <div class ="gitHub"> <a  href="https://github.com/CoryHrycko"><i class="fa fa-github fa-lg" aria-hidden="true" id="githubIcon"></i>Github  </a></div>
+        &nbsp&nbsp&nbsp
+         <div class ="linkedIn"> <a  href="https://www.linkedin.com/in/cory-hrycko-196802107/"><i class="fa fa-linkedin fa-lg" aria-hidden="true" id="linkedinIcon"></i>LinkedIn  </a></div>
+         &nbsp&nbsp&nbsp
+         <div class ="faceBook"> <a  href="https://www.facebook.com/cory.hrycko"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"id="facebookIcon"></i>FaceBook  </a></div>
+     </div>
+@endsection
