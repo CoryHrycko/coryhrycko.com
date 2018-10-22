cd2925d89f78dd48ff530d3cc89ad797f0370757
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-22 00:49:25 -0400

    submitting changes for the primary website reshaped all of te css and modulated.

--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,19 @@
-$(window).scroll(function(){
+function myFunction() {
 	console.log('hi')
-)};
+};
+
+window.onscroll = function() {myFunction()};
+
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.L = "Pause";
+  } else {
+    video.pause();
+    btn.textcontent = "Play";
+  }
+}
